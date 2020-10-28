22e4dd7a2e142e896502b941be1408dc9ed228c8
irwinchyi <irwinchyi@live.com>
2020-10-27 22:51:26 -0500

    PushNew

--- sketch.js
+++ sketch.js
@@ -3,16 +3,17 @@ let mainc;
 //scene size is bigger than the camera
 let SCENE_W = 2500; //the size of the whole scene
 let SCENE_H = 2500;
-let boxa,boxb,boxc,boxd,boxe,boxf,boxg,boxh,boxi,boxj,boxk,boxl,boxm,noo,never,whore,nobody,vain,freak,youmup;
+let boxa,boxb,boxc,boxd,boxe,boxf,boxg,boxh,boxi,boxj,boxk,boxl,boxm,noo,never,whore,nobody,vain,freak,youmup,aweirdo,you,everyone;
+let stopv,resources;
 
 function preload() {
     bg = createSprite(800,800,1600,1600);
-    mainc = createSprite(0,0,30,50)
-    let myAnimation = mainc.addAnimation('floating', 'assets/what/what001.png', 'assets/what/what003.png');
+    mainc = createSprite(30,30,30,30)
+    let myAnimation = mainc.addAnimation('floating', 'assets/walking/1.png', 'assets/walking/5.png');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
     bg.addImage(loadImage('assets/background.png'));
     
     //the following sprite is coll
@@ -96,13 +97,39 @@ function setup() {
     freak = createSprite(1010,813,448,113);
     freak.addImage(loadImage('assets/FREAK.png'));
     freak.setCollider('rectangle',0,0,448,113);
+    
+    youmup = createSprite(1463,140,214,229);
+    youmup.addImage(loadImage('assets/youmup.png'));
+    youmup.setCollider('rectangle',0,0,214,229);
+    
+    aweirdo = createSprite(1412,669,339,78);
+    aweirdo.addImage(loadImage('assets/AWEIRDO.png'));
+    aweirdo.setCollider('rectangle',0,0,339,78);
+    
+    you = createSprite(1069,1181,141,105);
+    you.addImage(loadImage('assets/you.png'));
+    you.setCollider('rectangle',0,0,141,105);
+    
+    uncool = createSprite(1109,1416,220,160);
+    uncool.addImage(loadImage('assets/uncool.png'));
+    uncool.setCollider('rectangle',0,0,220,160);
+    
+    everyone = createSprite(295,1184,168,74);
+    everyone.addImage(loadImage('assets/everyone.png'));
+    everyone.setCollider('rectangle',0,0,168,74);
+    
+    stopv = createSprite(1900,1900);
+    stopv.addImage(loadImage('assets/stopverbalabuse.png'));
+    
+    resources = createSprite(2500,1900);
+    resources.addImage(loadImage('assets/resources.png'));
 }
 
 function draw() {
     background('black');
     
     mainc.velocity.x = (camera.mouseX-mainc.position.x)/20;
-    mainc.velocity.y = (camera.mouseY-mainc.position.y)/20;
+    mainc.velocity.y = (camera.mouseY-mainc.position.y-40)/20;
     camera.position.x = mainc.position.x;
     camera.position.y = mainc.position.y;
     drawSprites();
@@ -125,10 +152,15 @@ function draw() {
     mainc.displace(nobody);
     mainc.displace(vain);
     mainc.displace(freak);
+    mainc.displace(youmup);
+    mainc.displace(aweirdo);
+    mainc.displace(you);
+    mainc.displace(uncool);
+    mainc.displace(everyone);
     
     push();
     strokeWeight(4);
-    stroke(255, 204, 100);
+    stroke('white');
     noFill();
     rect(0,0,1600,1600);
     pop();
