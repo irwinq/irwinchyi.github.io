17074d4cfe33d79babda44176b4c94da0a0bcd49
irwinchyi <irwinchyi@live.com>
2021-04-19 14:52:37 -0500

    Alls-ICONS

---
+++ alls-icon/scripts/index.js
@@ -0,0 +1,64 @@
+var animation1 = bodymovin.loadAnimation({
+  container: document.getElementById('clock'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'clockjson.json'
+})
+
+var animation2 = bodymovin.loadAnimation({
+  container: document.getElementById('pen'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'pen.json'
+})
+
+var animation3 = bodymovin.loadAnimation({
+  container: document.getElementById('hos'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'hos.json'
+})
+
+var animation4 = bodymovin.loadAnimation({
+  container: document.getElementById('chat'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'chat.json'
+})
+
+var animation5 = bodymovin.loadAnimation({
+  container: document.getElementById('network'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'network.json'
+})
+
+var animation6 = bodymovin.loadAnimation({
+  container: document.getElementById('nurse'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'nurse.json'
+})
+
+var animation7 = bodymovin.loadAnimation({
+  container: document.getElementById('todolist'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'todolist.json'
+})
+
+var animation8 = bodymovin.loadAnimation({
+  container: document.getElementById('chart'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'chart.json'
+})
+
