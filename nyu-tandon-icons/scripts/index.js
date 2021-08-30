99f34be4b6f9594ee91cf066f35f5906b7c5459d
Irwin Qi <wqi3@irwins-mbp-2.myfiosgateway.com>
2021-08-30 01:27:11 -0400

    Push

---
+++ nyu-tandon-icons/scripts/index.js
@@ -0,0 +1,64 @@
+var animation1 = bodymovin.loadAnimation({
+  container: document.getElementById('clock'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'user experience.json'
+})
+
+var animation2 = bodymovin.loadAnimation({
+  container: document.getElementById('pen'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data visualization.json'
+})
+
+var animation3 = bodymovin.loadAnimation({
+  container: document.getElementById('hos'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'interaction design.json'
+})
+
+var animation4 = bodymovin.loadAnimation({
+  container: document.getElementById('chat'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'motion capture.json'
+})
+
+var animation5 = bodymovin.loadAnimation({
+  container: document.getElementById('network'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'share.json'
+})
+
+var animation6 = bodymovin.loadAnimation({
+  container: document.getElementById('nurse'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'marketing.json'
+})
+
+var animation7 = bodymovin.loadAnimation({
+  container: document.getElementById('todolist'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'marketing.json'
+})
+
+var animation8 = bodymovin.loadAnimation({
+  container: document.getElementById('chart'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'chart.json'
+})
+
