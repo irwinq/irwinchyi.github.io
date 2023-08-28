ea292867d21d27ce80ac6703e594add127e398f4
irwinchyi <wangshuo.qi@nyulangone.org>
2023-08-28 14:42:55 -0400

    Update

---
+++ itdtp/itdtp3d/src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/T82pE4seNY-mdvuP/scene.splinecode');
